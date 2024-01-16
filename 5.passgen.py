c83f5ae0a421e4811dfdbeba6c0b45559c590e98
macydnah <juande.278@gmail.com>
2024-01-16 16:05:29 -0600

    refactor(lesson5): modify `pattern` variable
    The dot character is a wildcard that matches any character in a string —
    except for a newline character by default.
    
    - Modify `pattern` to match the entire string
    
    - Use a `.` followed by the `+` quantifier

--- 5.passgen.py
+++ 5.passgen.py
@@ -16,17 +16,18 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
+        
         constraints = [
             (nums, '[0-9]'),
             (lowercase, '[a-z]'),
             (uppercase, '[A-Z]'),
             (special_chars, '')
         ]        
+
     return password
     
 # new_password = generate_password(8)
 # print(new_password)
-
-pattern = '[^a-z]t'
+pattern = '.+'
 quote = 'Not all those who wander are lost.'
-# print(re.findall(pattern, quote))
+print(re.findall(pattern, quote))
