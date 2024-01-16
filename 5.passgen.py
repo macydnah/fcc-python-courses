668a53a0886053b42215aea8ae94a9c7acc3bc2c
macydnah <juande.278@gmail.com>
2024-01-16 17:48:57 -0600

    feat(lesson5): add `for` to i over `constraints`
    - Write a `for` loop to iterate over the `constraints` list
    
    - Use `constraint` and `pattern` as the iterating variables

--- 5.passgen.py
+++ 5.passgen.py
@@ -16,14 +16,16 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
+       
         constraints = [
             (nums, r'\d'),
             (lowercase, r'[a-z]'),
             (uppercase, r'[A-Z]'),            
-            (special_chars, fr'[{symbols}]')
-            # Check constraints
+            (special_chars, fr'[{symbols}]')            
         ]
-        
+        # Check constraints
+        for constraint, pattern in constraints:
+            pass
     return password
 
 # new_password = generate_password(8)
