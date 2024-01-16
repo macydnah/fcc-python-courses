86fdd11092f715207eeea5f88cac0e8a3b1099bb
macydnah <juande.278@gmail.com>
2024-01-16 15:12:06 -0600

    feat(lesson5): declare `quote` variable
    The search() function from the `re` module analyzes the string passed as
    the argument looking for the first place where the regex pattern matches
    the string.
    
    - Declare a variable called `quote` and assign the string "Not all those
      who wander are lost." to this variable. Then, print the result of
      pattern.search(quote).

--- 5.passgen.py
+++ 5.passgen.py
@@ -26,3 +26,5 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
 # new_password = generate_password(8)
 # print(new_password)
 pattern = re.compile('i')
+quote = 'Not all those who wander are lost.'
+print(pattern.search(quote))
