88c8ffd10d6db993450ba7aa8d5d0832f6df3b57
macydnah <juande.278@gmail.com>
2024-01-16 14:19:46 -0600

    refactor(lesson5): add `generate_password()` func
    Declare a generate_password function and write all your code except
    the import lines inside the function body.

--- 5.passgen.py
+++ 5.passgen.py
@@ -1,10 +1,11 @@
 import secrets
 import string
 
-# Define the possible characters for the password
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
+def generate_password():
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
 
-# Combine all characters
-all_characters = letters + digits + symbols
+    # Combine all characters
+    all_characters = letters + digits + symbols
