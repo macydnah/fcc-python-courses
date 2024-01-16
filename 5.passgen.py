dc983be1cf83b3cf13b56d31bb69301abf267db4
macydnah <juande.278@gmail.com>
2024-01-16 15:46:14 -0600

    refactor(lesson5): modify `nums` tuple to use `-`
    Modify the pattern in constraint tuple to indicate the range of all
    digits using square brackets.

--- 5.passgen.py
+++ 5.passgen.py
@@ -16,15 +16,14 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
-        
         constraints = [
-            (nums, '[0123456789]')
+            (nums, '[0-9]')
         ]        
-
     return password
     
 # new_password = generate_password(8)
 # print(new_password)
+
 pattern = '[a-z]t'
 quote = 'Not all those who wander are lost.'
-print(re.findall(pattern, quote))
+#print(re.findall(pattern, quote))
