438fcf4d260905fa53a64b2512f1cf28d3c21839
macydnah <juande.278@gmail.com>
2024-01-16 16:09:10 -0600

    refactor(lesson5): modify `pattern` variable
    Modify `pattern` so that it matches a single literal dot.

--- 5.passgen.py
+++ 5.passgen.py
@@ -28,6 +28,6 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
     
 # new_password = generate_password(8)
 # print(new_password)
-pattern = '.+'
+pattern = '\.'
 quote = 'Not all those who wander are lost.'
 print(re.findall(pattern, quote))
