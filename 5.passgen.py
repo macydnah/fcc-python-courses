de8504cb086d3dad5fd4afef732460d1c39ee24c
macydnah <juande.278@gmail.com>
2024-01-16 15:19:39 -0600

    feat(lesson5): add `+` quantifier to `pattern`

--- 5.passgen.py
+++ 5.passgen.py
@@ -24,7 +24,7 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
     return password
     
 # new_password = generate_password(8)
-# print(new_password)    
-pattern = re.compile('l')
+# print(new_password)
+pattern = re.compile('l+')
 quote = 'Not all those who wander are lost.'
 print(pattern.search(quote))
