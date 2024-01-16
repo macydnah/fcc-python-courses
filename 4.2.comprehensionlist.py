8efe19d2b393bb33080d0b4c34dcc60a87cfda0c
macydnah <juande.278@gmail.com>
2024-01-16 14:04:23 -0600

    refactor(lesson4): add a PascalCase example too
    Lesson 4. Adding a forgotten PascalCase example along with the camelCase
    example that goes with the case converter program.

--- 4.2.comprehensionlist.py
+++ 4.2.comprehensionlist.py
@@ -9,7 +9,8 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     return ''.join(snake_cased_char_list).strip('_')
 
 def main():
-    print(convert_to_snake_case('aLongAndComplexString'))
+    #print(convert_to_snake_case('aLongAndComplexString'))
+    print(convert_to_snake_case('IAmAPascalCasedString'))
 
 if __name__ == '__main__':
     main()
