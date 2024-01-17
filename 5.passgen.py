62c7e552c60554ab65ce797994ac59671686813a
macydnah <juande.278@gmail.com>
2024-01-16 18:04:45 -0600

    feat(lesson5): turn comparison to `if` statement
    - Turn the expression inside `for` loop into an `if` statement
    
    - Increment the `count` value by `1`, inside the new conditional
      statement

--- 5.passgen.py
+++ 5.passgen.py
@@ -26,7 +26,8 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Check constraints
         count = 0
         for constraint, pattern in constraints:
-            constraint <= len(re.findall(pattern, password))
+            if constraint <= len(re.findall(pattern, password)):
+                count += 1
     return password
 
 # new_password = generate_password(8)
