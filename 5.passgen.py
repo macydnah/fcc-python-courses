8c4efe7481d8065c8d66fad38bd3fc9e460e8bad
macydnah <juande.278@gmail.com>
2024-01-17 18:17:12 -0600

    refactor(lesson5): del `generate_password()` args
    - Remove all the arguments from the `generate_password()` call

--- 5.passgen.py
+++ 5.passgen.py
@@ -33,5 +33,5 @@ def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase
     
     return password
     
-new_password = generate_password(length=8)
+new_password = generate_password()
 print(new_password)
