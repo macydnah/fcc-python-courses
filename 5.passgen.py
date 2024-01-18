38f89d6a2ea247294245beb2f4c9124a1fec0a96
macydnah <juande.278@gmail.com>
2024-01-17 18:03:56 -0600

    style(lesson5): increase readability of func call
    - Modify `generate_password()` call to use keyword arguments

--- 5.passgen.py
+++ 5.passgen.py
@@ -3,6 +3,7 @@ import secrets
 import string
 
 def generate_password(length, nums, special_chars, uppercase, lowercase):
+   
     # Define the possible characters for the password
     letters = string.ascii_letters
     digits = string.digits
@@ -16,22 +17,22 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
-       
+        
         constraints = [
             (nums, r'\d'),
-            (lowercase, r'[a-z]'),
-            (uppercase, r'[A-Z]'),            
-            (special_chars, fr'[{symbols}]')            
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
         ]
-        
+
         # Check constraints        
         if all(
             constraint <= len(re.findall(pattern, password))
             for constraint, pattern in constraints
         ):
             break
-
+    
     return password
     
-new_password = generate_password(8, 1, 1, 1, 1)
+new_password = generate_password(length=8, nums=1, special_chars=1, uppercase=1, lowercase=1)
 print(new_password)
