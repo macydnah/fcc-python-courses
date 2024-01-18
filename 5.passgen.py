2dd7340075c6df2a55c75d9722b827dc656903d8
macydnah <juande.278@gmail.com>
2024-01-17 18:25:54 -0600

    feat(lesson5): add `if __name__ == '__main__'`
    Password generator is complete.
    
    - Put `generate_password()` and `print()` calls inside `if` statement that
    execute when `__name__ == '__main__'`

--- 5.passgen.py
+++ 5.passgen.py
@@ -3,6 +3,7 @@ import secrets
 import string
 
 def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+
     # Define the possible characters for the password
     letters = string.ascii_letters
     digits = string.digits
@@ -33,5 +34,6 @@ def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase
     
     return password
     
-new_password = generate_password()
-print('Generated password:', new_password)
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
