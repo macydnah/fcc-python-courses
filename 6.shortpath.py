fc878ca994c26d1010a8e7b03ec237f64eddc138
macydnah <juande.278@gmail.com>
2024-01-18 16:09:53 -0600

    refactor(lesson6): remove `for` loop with its body
    All the distances in `distances` are set to infinite, except for the
    starting node. The `unvisited` list contains all the nodes in the graph.
    But the `for` loop isn't needed to achieve this result.
    
    - Remove `for` loop with its entire body

--- 6.shortpath.py
+++ 6.shortpath.py
@@ -8,12 +8,6 @@ my_graph = {
 def shortest_path(graph, start):
     unvisited = []
     distances = {}
-    for node in graph:
-        unvisited.append(node)
-        if node == start:
-            distances[node] = 0
-        else:
-            distances[node] = float('inf')
     print(f'Unvisited: {unvisited}\nDistances: {distances}')
-
+    
 shortest_path(my_graph, 'A')
