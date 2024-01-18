02555954fdcc7e181262f14a8e656ad39d79b76e
macydnah <juande.278@gmail.com>
2024-01-18 16:06:30 -0600

    test(lesson6): call `shortest_path()` function
    - Pass `my_graph` and `'A'` as function arguments

--- 6.shortpath.py
+++ 6.shortpath.py
@@ -15,3 +15,5 @@ def shortest_path(graph, start):
         else:
             distances[node] = float('inf')
     print(f'Unvisited: {unvisited}\nDistances: {distances}')
+
+shortest_path(my_graph, 'A')
