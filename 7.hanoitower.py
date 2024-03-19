b13fe4917d191237258b0db94a6a598eab4ca048
macydnah <juande.278@gmail.com>
2024-03-19 17:47:11 -0600

    test(lesson7): remove `print()` call
    - Inside `move()` function, remove `pass` and print the contents of
      `rods` dictionary

--- 7.hanoitower.py
+++ 7.hanoitower.py
@@ -1,11 +1,9 @@
 NUMBER_OF_DISKS = 3
 number_of_moves = 2**NUMBER_OF_DISKS - 1
-print(number_of_moves)
 rods = {
     'A': list(range(NUMBER_OF_DISKS, 0, -1)),
     'B': [],
     'C': []
 }
-
 def move():
-    pass
+    print(rods)
