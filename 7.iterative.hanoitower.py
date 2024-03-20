4de997cd64bf8a0ff4979087b43ff6595cca6eff
macydnah <juande.278@gmail.com>
2024-03-20 14:18:25 -0600

    feat(lesson7): check `if` `rods[target]` is empty
    When `target` is empty, the disk should be moved necessarily from
    `source` to `target`. After the declaration of `forward`:
    
    - Add an `if` statement to check if `rods[target]` is empty. If it is,
      change `forward` to `True`

--- 7.iterative.hanoitower.py
+++ 7.iterative.hanoitower.py
@@ -13,12 +13,13 @@ def move(n, source, auxiliary, target):
         remainder = (i + 1) % 3
         if remainder == 1:
             print(f'Move {i + 1} allowed between {source} and {target}')
+            forward = False
+            if not rods[target]:
+                forward = True
         elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
         elif remainder == 0:
             print(f'Move {i + 1} allowed between {auxiliary} and {target}')
-
-    forward = False
 
 # initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
