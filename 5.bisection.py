2f2362dce817ccdf3c9e22c38b4c46b4e637ee2d
macydnah <juande.278@gmail.com>
2024-10-18 14:46:13 -0600

    lesson5b: `root` to `mid` if diff within `tolerance`

--- 5.bisection.py
+++ 5.bisection.py
@@ -15,4 +15,5 @@ def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
             mid = (low + high) / 2
             square_mid = mid ** 2
             if abs(square_mid - square_target) < tolerance:
-                pass
+                root = mid
+                break
