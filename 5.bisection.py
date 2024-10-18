a67e21409c288b42d4ce496f496fb5d21ee2414c
macydnah <juande.278@gmail.com>
2024-10-18 14:06:56 -0600

    lesson5b: separate 0 or 1 cases to `square_target`

--- 5.bisection.py
+++ 5.bisection.py
@@ -1,3 +1,5 @@
 def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
     if square_target < 0:
         raise ValueError("Square root of negative number is not defined in real numbers")
+    if square_target == 1:
+        pass
