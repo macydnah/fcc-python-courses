cd9c325ae8fa3b0043a6b607fad05f9217bc3188
macydnah <juande.278@gmail.com>
2024-10-18 14:24:50 -0600

    add answer to `square_target` equal to 0

--- 5.bisection.py
+++ 5.bisection.py
@@ -4,3 +4,6 @@ def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
     if square_target == 1:
         root = 1
         print(f'The square root of {square_target} is 1')
+    elif square_target == 0:
+        root = 0
+        print(f'The square root of {square_target} is 0')
