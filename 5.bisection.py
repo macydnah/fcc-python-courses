8f06b79fb039b3d67d119f84cbf16eba87930931
macydnah <juande.278@gmail.com>
2024-10-18 14:26:12 -0600

    lesson5b: handle other cases with `else`

--- 5.bisection.py
+++ 5.bisection.py
@@ -7,3 +7,5 @@ def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
     elif square_target == 0:
         root = 0
         print(f'The square root of {square_target} is 0')
+    else:
+        pass
