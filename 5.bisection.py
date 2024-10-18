99ea77518b58ce007dd6c40c8c8443edd5e63f6b
macydnah <juande.278@gmail.com>
2024-10-18 14:03:44 -0600

    lesson5b: `raise` a `ValueError` with invalid nums

--- 5.bisection.py
+++ 5.bisection.py
@@ -1,3 +1,3 @@
 def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
     if square_target < 0:
-        pass
+        raise ValueError("Square root of negative number is not defined in real numbers")
