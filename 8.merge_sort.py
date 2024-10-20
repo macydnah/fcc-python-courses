5c652df7d422c93d00a3de546e11e2d57249dc39
macydnah <juande.278@gmail.com>
2024-10-20 01:27:51 -0600

    lesson8: pass `left_part` as `merge_sort` argument

--- 8.merge_sort.py
+++ 8.merge_sort.py
@@ -1,5 +1,7 @@
 def merge_sort(array):
+
     middle_point = len(array) // 2
     left_part = array[:middle_point]
     right_part = array[middle_point:]
-    merge_sort()
+
+    merge_sort(left_part)
