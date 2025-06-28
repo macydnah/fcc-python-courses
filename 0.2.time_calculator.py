16e747e914e0d7ca08022b4d7c5e5d3c41b7995e
macydnah <86342863+macydnah@users.noreply.github.com>
2025-06-28 01:13:59 -0600

    refactor(0.2.time_calculator): `fix_overflow_in()`
    Certification Project: `time_calculator.py`
    
    - Unify `fix_overflow()` and `fix_overflOw() functions

--- 0.2.time_calculator.py
+++ 0.2.time_calculator.py
@@ -1,10 +1,9 @@
-def final_print():
+def resultado():
     print(f'\n\
-the new_time final result is: {\
+Final result: {\
             add_time(\
             test_start,\
             test_duration,\
-            # test_weekday if test_weekday else None\
             test_weekday
             )\
             }\
@@ -20,13 +19,13 @@ test_weekday = ''
 # test_start, test_duration = '3:00 PM', '3:10'
 # test_start, test_duration, test_weekday = '11:30 AM', '2:32', 'Monday'
 # test_start, test_duration = '10:10 PM', '3:30'
-# test_start, test_duration, test_weekday = '11:43 PM', '24:20', 'tueSday'
-test_start, test_duration = '6:30 PM', '205:12'
+test_start, test_duration, test_weekday = '11:43 PM', '24:20', 'tueSday'
+# test_start, test_duration = '6:30 PM', '205:12'
 
-time = {
-    'hour': int(0),
-    'minute': int(0),
-    'meridiem': ['AM', 'PM'],
+TIME = {
+    'hour': int(),
+    'minute': int(),
+    'meridiem': str(),
     'day': [
         'Monday',
         'Tuesday',
@@ -39,9 +38,37 @@ time = {
 }
 
 DURATION = {
-    'hour': int(0),
-    'minute': int(0)
+    'hour': int(),
+    'minute': int()
 }
+
+def init(time_str, day=None):
+    """Parse a time string in the format 'HH:MM AM/PM' or 'HH:MM'."""
+    h_m_M = time_str.strip().strip("',").split()
+
+    if len(h_m_M) > 1: # then it's a start time with AM/PM
+        # save the meridiem before splitting
+        TIME['meridiem'] = str(h_m_M[1]).upper()
+
+        h_m = h_m_M[0].split(':')
+        TIME['hour'] = int(h_m[0])
+        TIME['minute'] = int(h_m[1])
+
+    else: # it's a DURATION[] time without AM/PM
+        h_m = h_m_M[0].split(':')
+        DURATION['hour'] = int(h_m[0])
+        DURATION['minute'] = int(h_m[1])
+
+
+def how_many(days_later):
+    """Return a string indicating how many time_objects later."""
+    if days_later == 0:
+        print(f'how_many():66 is {days_later} == 0 reachable')
+        return
+    elif days_later == 1:
+        return ' (next day)'
+    else:
+        return f' ({days_later} days later)'
 
 def clock_12_to_24(hour, meridiem):
     """Convert 12-hour format to 24-hour format."""
@@ -62,111 +89,89 @@ def clock_24_to_12(hour):
     else:
         return hour % 12, 'PM'
 
-def how_many(time_objects):
-    """Return a string indicating how many time_objects later."""
-    if time_objects == 0:
-        return
-    elif time_objects == 1:
-        return " (next day)"
-    else:
-        return f" ({time_objects} days later)"
-
-def fix_0B3R4l0w(minute):
+def fix_overflow(minute):
     """Check if minutes overflow and adjust hours accordingly."""
     if minute >= 60:
-        print(f'check_0B3R4l0w():77 Overfl0w detected: {minute} minutes')
         return minute % 60, minute // 60
     return minute, 0
 
-def fix_0B3R4l0w_days(new_hour):
+def fix_overflOw(hour):
     """Calculate the number of overflow days based on the new hour."""
-    if new_hour >= 24:
-        overflow_days = new_hour // 24
-        new_hour = new_hour % 24
-        return new_hour, overflow_days
-    return new_hour, 0
+    if hour >= 24:
+        overflow_days = hour // 24
+        hour = hour % 24
+        return hour, overflow_days
+    return hour, 0
 
-def parse(time_str, day=None):
-    """Parse a time string in the format 'HH:MM AM/PM' or 'HH:MM'."""
-    h_m_M = time_str.strip().strip("',").split()
+# def fix_overflow_in(hour=None, minute=None):
+#     """Check if hours overflow and adjust days accordingly."""
+#     if hour:
+#         if hour >= 24:
+#             overflow_days = hour // 24
+#             hour = hour % 24
+#             return hour, overflow_days
+#         return hour, 0
+#
+#     if minute:
+#         if minute >= 60:
+#             return minute % 60, minute // 60
+#         return minute, 0
 
-    if day:
-        day = day.lower().capitalize()
-        # print(f'parse():95 time["day"] type: {type(time["day"])}')
-        # time['day'] = time['day'].index(day)
-        # print(f'parse():97 time["day"]: {time["day"]}')
-        # print(f'parse():98 time["day"] type: {type(time["day"])}')
+def fix_overflow_in(time, to = {'over_hours', 'days_later'}):
+    """Check if minutes, hours, days overflow and adjust accordingly."""
+    print(f'fix_overflow_in():104 {time}, for_what: {to}')
+    print(f'fix_overflow_in():105 time type: {type(time)} and for_what type: {type(to)}')
 
-    if len(h_m_M) > 1: # then it's a start time with AM/PM
-        # save the meridiem before splitting
-        print(f'parse():102 time["meridiem"]: {time["meridiem"]}')
-        print(f'parse():103 time["meridiem"] tyep: {type(time["meridiem"])}')
-        time['meridiem'] = h_m_M[1]
-        print(f'parse():105 time["meridiem"]: {time["meridiem"]}')
-        print(f'parse():106 time["meridiem"] type: {type(time["meridiem"])}')
+    what_to_fix = to
 
-        h_m = h_m_M[0].split(':')
-        time['hour'] = int(h_m[0])
-        time['minute'] = int(h_m[1])
+    if what_to_fix == {'days_later'}:
+        hour = time
+        if hour >= 24:
+            overflow_days = hour // 24
+            hour = hour % 24
+            return hour, overflow_days
+        return hour, 0
 
-    else: # it's a DURATION[] time without AM/PM
-        h_m = h_m_M[0].split(':')
-        DURATION['hour'] = int(h_m[0])
-        DURATION['minute'] = int(h_m[1])
+    if what_to_fix == {'over_hours'}:
+        minute = time
+        if minute >= 60:
+            overflow_hours = minute // 60
+            minute = minute % 60
+            return minute, overflow_hours
+        return minute, 0
+
 
 def add_time(start, duration, day=None):
-    # if day:
-    #     day = day.lower().capitalize()
-    #     start_hour, start_minute, meridiem, start_day = parse(start, day)
-    # else:
-    #     start_hour, start_minute, meridiem = parse(start)
-    #
-    # print(f'\nStart hour: {start_hour}, Start minute: {start_minute}, Start period: {meridiem}, Start day: {start_day if day else "Day not provided"}')
-    # print(f'Start day label: {time["day"][start_day] if day else "No day provided"}')
-    #
-    # hour_duration, minute_duration = parse(duration)
-
     if day:
         day = day.lower().capitalize()
-        parse(start, day)
-        start_day = time['day'].index(day)
+        init(start, day)
+        start_day = TIME['day'].index(day)
     else:
-        parse(start)
+        init(start)
+    init(duration)
 
-    print(f'Start hour: {time["hour"]}, Start minute: {time["minute"]}, Start period: {time["meridiem"]}, Start day: {time["day"] if day else "Day not provided"}')
+    print(f'Start hour: {TIME["hour"]}, Start minute: {TIME["minute"]}, Start period: {TIME["meridiem"]}, Start day: {TIME["day"] if day else "Day not provided"}')
 
-    parse(duration)
+    TIME['hour'] = clock_12_to_24(TIME['hour'], TIME['meridiem'])
 
-    print(f'time["meridiem"]: {time["meridiem"]}')
-    time['hour'] = clock_12_to_24(time['hour'], time['meridiem'])
+    TIME['hour'] += DURATION['hour']
+    TIME['minute'] += DURATION['minute']
+    # TIME['minute'], over_hours = fix_overflow(TIME['minute'])
+    TIME['minute'], over_hours = fix_overflow_in(TIME['minute'], to={'over_hours'})
 
-    time['hour'] += DURATION['hour']
-    time['minute'] += DURATION['minute']
-    time['minute'], over_hours = fix_0B3R4l0w(time['minute'])
+    TIME['hour'] += over_hours
+    # TIME['hour'], days_later = fix_overflOw(TIME['hour'])
+    TIME['hour'], days_later = fix_overflow_in(TIME['hour'], to={'days_later'})
 
-    time['hour'] += over_hours
-    time['hour'], days_later = fix_0B3R4l0w_days(time['hour'])
-    print(f'add_time():149 days_later: {days_later}')
-    print(f'add_time():150 days_later type: {type(days_later)}')
+    TIME['hour'], TIME['meridiem'] = clock_24_to_12(TIME['hour'])
 
-    time['hour'], time['meridiem'] = clock_24_to_12(time['hour'])
-
+    new_time = f'{TIME["hour"]}:{TIME["minute"]:02d} {TIME["meridiem"]}'
     if day:
-        print(f'add_time():153 time["day"].index(day) type: {type(time["day"].index(day))}')
-        print(f'time["day"]: {time["day"]}')
-        print(f'{time["day"].index(day)}')
-        # time['day'] += time['day'][(start_day + days_later) % 7]
-        # time['day'] = time['day'][(time['day'].index(day) + days_later) % 7]
-    print(f'add_time():160 time after conversion: {time}')
-
-
-    new_time = f'{time["hour"]}:{time["minute"]:02d} {time["meridiem"]}'
-    if day:
-        new_time += f', {time["day"][(start_day + days_later) % 7]}'
+        new_time += f', {TIME["day"][(start_day + days_later) % 7]}'
     if days_later > 0:
         new_time += how_many(days_later)
 
     return new_time
 
 if __name__ == '__main__':
-    final_print()
+    resultado()
