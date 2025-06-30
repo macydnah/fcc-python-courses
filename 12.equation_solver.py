9530db49974f9435007ae788deede36f5f191f3f
macydnah <86342863+macydnah@users.noreply.github.com>
2025-06-29 23:37:09 -0600

    lesson12: `import abc`
    `ABC` stands for _Abstract Base Classes_. The `ABC` class enables to turn
    a regular class into an abstract class, which is a class that acts as a
    blueprint for concrete classes.

--- 12.equation_solver.py
+++ 12.equation_solver.py
@@ -1,2 +1,14 @@
+import abc
+
 class Equation:
+    def solve(self):
+        pass
+
+    def analyze(self):
+        pass
+
+class LinearEquation(Equation):
     pass
+
+eq = Equation()
+lin_eq = LinearEquation()
